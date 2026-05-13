f337d4b8395c5ca1d207c4d287085f06ac890848
Javier Gutiérrez Del Caso <javier.gutierrezd@alumnos.viu.es>
2026-05-13 20:51:54 +0200

    fix

--- terraform/modules/app_service/main.tf
+++ terraform/modules/app_service/main.tf
@@ -12,7 +12,6 @@ resource "azurerm_linux_web_app" "app" {
   resource_group_name = var.resource_group
   service_plan_id     = azurerm_service_plan.plan.id
 
-  # HTTPS obligatorio (correcto en AzureRM 4.x)
   https_only = true
 
   site_config {
@@ -20,15 +19,12 @@ resource "azurerm_linux_web_app" "app" {
       dotnet_version = "8.0"
     }
 
-    # FTP deshabilitado
-    ftps_state = "Disabled"
-
-    # Health check (ambos requeridos)
-    health_check_path                  = "/"
-    health_check_eviction_time_in_min  = 10
+    ftps_state                     = "Disabled"
+    health_check_path              = "/"
+    health_check_eviction_time_in_min = 10
   }
 
   app_settings = {
-    "APP_ENV" = var.app_env
+    APP_ENV = var.app_env
   }
 }
