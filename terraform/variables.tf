d51da85741cc8cb71dd0af3550da4ca7a93d3ad5
Javier Gutiérrez Del Caso <javier.gutierrezd@alumnos.viu.es>
2026-05-24 12:39:23 +0200

    fix

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -14,19 +14,23 @@ variable "location" {
 
 variable "arm_client_id" {
   type        = string
+  description = "Client ID del Service Principal."
 }
 
 variable "arm_client_secret" {
-  type      = string
-  sensitive = true
+  type        = string
+  sensitive   = true
+  description = "Client Secret del Service Principal."
 }
 
 variable "arm_tenant_id" {
-  type = string
+  type        = string
+  description = "Tenant ID del Service Principal."
 }
 
 variable "arm_subscription_id" {
-  type = string
+  type        = string
+  description = "Subscription ID del Service Principal."
 }
 
 ###############################################
@@ -34,16 +38,19 @@ variable "arm_subscription_id" {
 ###############################################
 
 variable "ghcr_token" {
-  type      = string
-  sensitive = true
+  type        = string
+  sensitive   = true
+  description = "Token GHCR para almacenar en Key Vault."
 }
 
 variable "api_key" {
-  type      = string
-  sensitive = true
+  type        = string
+  sensitive   = true
+  description = "API Key para almacenar en Key Vault."
 }
 
 variable "jwt_secret" {
-  type      = string
-  sensitive = true
+  type        = string
+  sensitive   = true
+  description = "JWT Secret para almacenar en Key Vault."
 }
