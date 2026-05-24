f343379e3dd6706dd941db236715104cefa2605e
Javier Gutiérrez Del Caso <javier.gutierrezd@alumnos.viu.es>
2026-05-24 12:29:14 +0200

    Fix

--- terraform/modules/keyvault/main.tf
+++ terraform/modules/keyvault/main.tf
@@ -8,46 +8,39 @@ resource "azurerm_key_vault" "this" {
   purge_protection_enabled   = true
   soft_delete_retention_days = 7
 
-  # Cumple tfsec: azure-keyvault-specify-network-acl
   network_acls {
     default_action = "Deny"
     bypass         = "AzureServices"
 
-    dynamic "ip_rules" {
+    dynamic "ip_rule" {
       for_each = var.allowed_ip_ranges
       content {
-        value = ip_rules.value
+        value = ip_rule.value
       }
     }
   }
 }
 
 resource "azurerm_key_vault_secret" "ghcr_token" {
-  name         = "GHCR-TOKEN"
-  value        = var.ghcr_token
-  key_vault_id = azurerm_key_vault.this.id
-
-  # Cumple tfsec: azure-keyvault-content-type-for-secret
-  content_type = "text/plain"
-
-  # Cumple tfsec: azure-keyvault-ensure-secret-expiry
+  name            = "GHCR-TOKEN"
+  value           = var.ghcr_token
+  key_vault_id    = azurerm_key_vault.this.id
+  content_type    = "text/plain"
   expiration_date = var.secrets_expiration_date
 }
 
 resource "azurerm_key_vault_secret" "api_key" {
-  name         = "API-KEY"
-  value        = var.api_key
-  key_vault_id = azurerm_key_vault.this.id
-
-  content_type   = "text/plain"
+  name            = "API-KEY"
+  value           = var.api_key
+  key_vault_id    = azurerm_key_vault.this.id
+  content_type    = "text/plain"
   expiration_date = var.secrets_expiration_date
 }
 
 resource "azurerm_key_vault_secret" "jwt_secret" {
-  name         = "JWT-SECRET"
-  value        = var.jwt_secret
-  key_vault_id = azurerm_key_vault.this.id
-
-  content_type   = "text/plain"
+  name            = "JWT-SECRET"
+  value           = var.jwt_secret
+  key_vault_id    = azurerm_key_vault.this.id
+  content_type    = "text/plain"
   expiration_date = var.secrets_expiration_date
 }
