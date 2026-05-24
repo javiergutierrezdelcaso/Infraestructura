9083ad7902121665908dcde4abfcd7b0e11c7140
Javier Gutiérrez Del Caso <javier.gutierrezd@alumnos.viu.es>
2026-05-24 15:18:42 +0200

    cambio

--- terraform/environments/pre/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "app_url" {
-  value       = module.app_service.app_url
-  description = "URL del App Service PRE"
-}
---
+++ terraform/pre/variables.tf
@@ -0,0 +1,38 @@
+variable "project" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
+
+variable "ghcr_token" {
+  type      = string
+  sensitive = true
+}
+
+variable "api_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "jwt_secret" {
+  type      = string
+  sensitive = true
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "log_analytics_workspace_id" {
+  type = string
+}
