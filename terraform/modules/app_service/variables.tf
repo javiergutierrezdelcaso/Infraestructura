fc1bbd34624c0798ca88806c34958eafc1596ffc
Javier Gutiérrez Del Caso <javier.gutierrezd@alumnos.viu.es>
2026-05-24 16:57:50 +0200

    cambios

--- terraform/modules/app_service/variables.tf
+++ terraform/modules/app_service/variables.tf
@@ -1,27 +1,5 @@
-variable "project" {
-  type = string
-}
-
-variable "environment" {
-  type = string
-}
-
-variable "location" {
-  type = string
-}
-
-variable "resource_group_name" {
-  type = string
-}
-
-variable "ghcr_token_secret_uri" {
-  type = string
-}
-
-variable "api_key_secret_uri" {
-  type = string
-}
-
-variable "jwt_secret_secret_uri" {
-  type = string
-}
+variable "project" { type = string }
+variable "environment" { type = string }
+variable "location" { type = string }
+variable "resource_group_name" { type = string }
+variable "sku_name" { type = string }
