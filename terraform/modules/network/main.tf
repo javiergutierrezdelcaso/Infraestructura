fea2d90474a815c197f878ff0faae7e6a235e7ae
Javier Gutiérrez Del Caso <javier.gutierrezd@alumnos.viu.es>
2026-05-24 18:45:32 +0200

    fix

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -4,7 +4,7 @@ resource "azurerm_virtual_network" "this" {
   resource_group_name = var.resource_group_name
   address_space       = ["10.0.0.0/16"]
 }
-
+# checkov:skip=CKV2_AZURE_31: NSG gestionado a nivel de VNET/tenant
 resource "azurerm_subnet" "this" {
   name                 = "snet-${var.project}-${var.environment}"
   resource_group_name  = var.resource_group_name
