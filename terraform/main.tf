cc83b449b344c6afa199844aa9d4c02a0b36a5db
Javier Gutiérrez Del Caso <javier.gutierrezd@alumnos.viu.es>
2026-05-24 13:24:31 +0200

    cambio

--- terraform/main.tf
+++ terraform/main.tf
@@ -126,14 +126,3 @@ resource "azurerm_private_endpoint" "kv_pe" {
   }
 }
 
-###############################################
-# OUTPUTS
-###############################################
-
-output "pre_app_url" {
-  value = module.app_service_pre.app_url
-}
-
-output "pro_app_url" {
-  value = module.app_service_pro.app_url
-}
